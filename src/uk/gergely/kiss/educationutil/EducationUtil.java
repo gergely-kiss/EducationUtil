6f8a5ad61e263b8fc54eb7ceecb3e2dbc944627e
gergely <kiss-gergely@outlook.com>
2018-11-07 10:25:14 +0000

    init commit

---
+++ src/uk/gergely/kiss/educationutil/EducationUtil.java
@@ -0,0 +1,10 @@
+package uk.gergely.kiss.educationutil;
+
+public class EducationUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/uk/gergely/kiss/educationutil/print/array/PrintArray.java
@@ -0,0 +1,5 @@
+package uk.gergely.kiss.educationutil.print.array;
+
+public class PrintArray {
+
+}
